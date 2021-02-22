2d77a12582edc618b006a20b6704a04baae4b820
Alina Bondarets <alinium3012@gmail.com>
2021-02-22 09:19:38 +0200

    Add working module

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """
 """
 import json
-from pprint import pprint
 
 
-def read_file(path: str):
+def read_file(path: str) -> dict:
     if not isinstance(path, str):
         return None
 
@@ -12,3 +11,41 @@ def read_file(path: str):
         data = json.load(f)
     return data
 
+
+def ask_user(obj) -> str:
+
+    if isinstance(obj, dict):
+        dict_keys = list(obj.keys())
+
+        if len(dict_keys) == 1:
+            return ask_user(obj[dict_keys[0]])
+
+        users_input = str(input(f'\nChoose the dictionary key:\n{dict_keys}\n'))
+        if users_input not in dict_keys:
+            return f'Incorrect key. Try again.\n'
+
+        return ask_user(obj[users_input])
+
+    elif isinstance(obj, list):
+
+        indexes = [num for num in range(len(obj))]
+        if len(indexes) > 1:
+            users_input = int(input(f'\nChoose the index of the list element:\n{indexes}\n'))
+
+            if users_input not in indexes:
+                return f'Incorrect key. Try again.\n'
+
+            return ask_user(obj[users_input])
+
+        elif len(indexes) == 1:
+            return ask_user(obj[0])
+        else:
+            return f'\nThe searched value is:\n{obj}\n'
+
+    else:
+        return f'\nThe searched value is:\n{obj}\n'
+    
+
+def main_func(path: str):
+    data = read_file(path)
+    print(ask_user(data))
